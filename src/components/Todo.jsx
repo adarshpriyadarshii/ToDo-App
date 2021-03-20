9d2f02ec5849f54b54705ee02e1df6b42ea9f7fb
ADARSH PRIYADARSHI <76248609+adarshpriyadarshii@users.noreply.github.com>
2021-03-20 09:58:18 +0530

    Add files via upload

---
+++ src/components/Todo.jsx
@@ -0,0 +1,12 @@
+import React,{useState} from "react";
+function Todo(props){
+   return (
+   <div 
+   onClick={()=>{
+       props.onChecked(props.id)
+       }}>
+        <li >{props.text}</li>
+    </div>
+    );
+}
+export default Todo;
